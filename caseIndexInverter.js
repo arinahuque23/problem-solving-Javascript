af4a0b6c0ce4036fb777da8c935b18e51a7d0ee8
arinahuque23 <arinahuqe23@gmail.com>
2024-08-09 18:04:48 +0600

    fibonacci sequence

--- caseIndexInverter.js
+++ caseIndexInverter.js
@@ -3,9 +3,9 @@ function invert(s) {
   // for (let i = 0; i <= s.length - 1; i++)
   for (let i = s.length - 1; i >= 0; i--) {
     result +=
-      s[i] === s[i].toLowerCase() ? s[i].toUpperCase() : s[i].toLowerCase();
+      s[i] == s[i].toLowerCase() ? s[i].toUpperCase() : s[i].toLowerCase();
   }
   return result;
 }
 
-console.log(invert("dLROW YM sI HsEt"));
+console.log(invert("step on NO PETS abc"));
